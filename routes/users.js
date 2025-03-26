8f89bec3956327fdb05b7268d4298cd0f595419d
Jeremy Murray <jeremy636m@gmail.com>
2025-03-26 15:46:13 -0500

    added value

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('value = ' + String(ratio));
 });
 
 module.exports = router;
+ratio = 0.75;
+acc = 1000;
+
