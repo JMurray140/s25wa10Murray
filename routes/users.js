bb9380e4d3a01e618f4bd42505adfa6a2efd5bce
Jeremy Murray <jeremy636m@gmail.com>
2025-03-26 15:48:26 -0500

    changed so it works now

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+ratio = 0.75;
+acc = 1000;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('value = ' + String(ratio));
+  acc = ratio * acc 
+  res.send('value = ' + String(acc));
 });
 
 module.exports = router;
-ratio = 0.75;
-acc = 1000;
 
